52304e22ec296d4bba746848116041839e196332
Claudiana Silva <110785598+ClaudiannaSilva@users.noreply.github.com>
2022-08-10 21:46:16 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\ProdutoSeader;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,8 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            ProdutoSeeder::class,
+        ]);   
         // \App\Models\User::factory(10)->create();
-
+        
+        
+    
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
